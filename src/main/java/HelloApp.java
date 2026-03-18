2f0dd1874f874a6734ae467cb01551e035d3a01b
avnibajpai <avnibajpai@Avnis-MacBook-Pro-5.local>
2026-03-18 15:46:16 +0530

    Display Hello with Multiple Command-Line Arguments

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,10 +1,19 @@
 public class HelloApp {
-
     public static void main(String[] args) {
-                String name = "World";
-       if(args.length > 0){
-           name = args[0];
-       }
-       System.out.println("Hello, " + name + "!");
+        StringBuilder namesBuilder = new StringBuilder();
+
+        if (args.length > 0) {
+            for (int i = 0; i < args.length; i++) {
+                namesBuilder.append(args[i]);
+                // Only add a comma if it's NOT the last element
+                if (i < args.length - 1) {
+                    namesBuilder.append(", ");
+                }
+            }
+        } else {
+            namesBuilder.append("World");
+        }
+
+        System.out.println("Hello, " + namesBuilder.toString() + "!");
     }
-} 
+}
